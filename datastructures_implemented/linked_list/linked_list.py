ac7a320a8a1f60f1368025cc427e473a78fb5c14
TimBLukas <tim.b.lukas@gmail.com>
2026-04-06 20:49:10 +0200

    added datastructures section

---
+++ datastructures_implemented/linked_list/linked_list.py
@@ -0,0 +1,45 @@
+"""
+Linked List:
+A singly linked list is a fundamental data structure,
+it consists of nodes where each node contains a data field and a
+reference to the next node in the linked list. The next of the last node is null,
+indicating the end of the list. Linked Lists support efficient insertion and deletion operations.
+
+https://www.geeksforgeeks.org/dsa/singly-linked-list-tutorial/
+
+Operations:
+- Traversal:
+- Insertion
+- Deletion
+- Searching
+- Updating
+- Reverseal
+"""
+
+
+class LinkedList:
+    def __init__():
+        pass
+
+    def append(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def iterate(self):
+        pass
+
+    def find(self):
+        pass
+
+    def update(self):
+        pass
+
+    def reverse(self):
+        pass
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        pass
