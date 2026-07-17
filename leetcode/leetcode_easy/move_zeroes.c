63037fc934a5024d5496150d0f7b12df32d30616
Tim Lukas <tim.b.lukas@gmail.com>
2026-07-17 12:42:56 +0200

    leetcode 283 move zeroes

---
+++ leetcode/leetcode_easy/move_zeroes.c
@@ -0,0 +1,48 @@
+/**
+ * Leetcode 283: Move Zeroes
+ * 
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ * Note that you must do this in-place without making a copy of the array.
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void moveZeroes(int* nums, int numsSize) 
+{
+    int i, j;
+    int added_zeroes = 0;
+    for (i=0; i < numsSize; i++)
+    {
+        if ( i >= numsSize - added_zeroes )
+            return;
+        else if ( nums[i] == 0 )
+        {
+            for (j=i + 1; j < numsSize; j++)
+            {
+                nums[j - 1] = nums[j];
+            }
+            nums[numsSize - 1] = 0;
+            added_zeroes++;
+            i--;
+        }
+    }
+}
+
+void moveZeroesTwo(int* nums, int numsSize) 
+{
+    int insert = 0;
+    for ( int i = 0; i < numsSize; i++)
+    {
+        if (i != 0)
+        {
+            nums[insert++] = nums[i];
+        }
+    }
+
+    for ( int j = insert; j < numsSize; j++ )
+    {
+        nums[j] = 0;
+    }
+}
