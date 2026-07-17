00f968139b92ecd5d348c9f33c5878c267cd3b45
Tim Lukas <tim.b.lukas@gmail.com>
2026-07-17 12:19:44 +0200

    leetcode 175 combine two tables

---
+++ leetcode/leetcode_easy/combine_two_tables.sql
@@ -0,0 +1,10 @@
+# Leetcode 175: Combine two tables
+
+# Write a solution to report the first name, last name, city, and state of each person in the Person table.
+# If the address of a personId is not present in the Address table, report null instead.
+# Return the result table in any order.
+
+
+SELECT Person.firstName, Person.lastName, Address.city, Address.state
+FROM Person
+LEFT OUTER JOIN Address ON Person.personId = Address.personId;
