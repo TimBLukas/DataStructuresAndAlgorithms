88634a0ba5cbc3c881775139035a5905fad82842
TimBLukas <tim.b.lukas@gmail.com>
2026-07-25 14:00:10 +0200

    leetcode 709: To Lower Case

---
+++ leetcode/leetcode_easy/to_lower_case.c
@@ -0,0 +1,22 @@
+/**
+* Leetcode 709: To Lower Case
+*
+* Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+*/
+
+#include <string.h>
+#include <stdlib.h>
+
+
+char* toLowerCase(char* s) 
+{
+        for ( int i = 0; i < strlen(s); i++ )
+        {
+                if ( s[i] >= 'A' && s[i] <= 'Z' )
+                {
+                        s[i] = 'a' + (s[i] - 'A');
+                }
+        }
+
+        return s;
+}
