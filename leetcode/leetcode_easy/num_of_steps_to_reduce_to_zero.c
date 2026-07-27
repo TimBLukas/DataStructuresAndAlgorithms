ee177abfe36c87be39c64f2ae90626a97dbb22de
TimBLukas <tim.b.lukas@gmail.com>
2026-07-27 15:22:42 +0200

    leetcode 1342: Number of steps to reduce a number to zero

---
+++ leetcode/leetcode_easy/num_of_steps_to_reduce_to_zero.c
@@ -0,0 +1,23 @@
+/**
+* Leetcode 1342: Number of steps to reduce a number to zero
+*
+* Given an integer num, return the number of steps to reduce it to zero.
+* In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int numberOfSteps(int num) 
+{
+        int cnt = 0;
+        while ( num > 0 )
+        {
+                if ( num % 2 == 0 )
+                        num /= 2;
+                else
+                        num -= 1;
+                cnt++;
+        }
+        return cnt;
+}
