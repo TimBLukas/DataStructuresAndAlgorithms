9c1a9716d603182478b47eed5e9f7af8ec8cb989
TimBLukas <tim.b.lukas@gmail.com>
2026-07-27 17:14:36 +0200

    Leetcode 31110: Score of a string

---
+++ leetcode/leetcode_easy/score_of_a_string.c
@@ -0,0 +1,24 @@
+/**
+* Leetcode 3110: Score of a String
+*
+* You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+*
+* Return the score of s.
+*/
+
+#include <stdlib.h>
+#include <string.h>
+
+
+int scoreOfString(char* s) 
+{
+        int score = 0;
+
+        for ( int i = 0; (i + 1) < strlen(s); i++)
+        {
+                score += abs(s[i] - s[i + 1]);
+        }
+
+        return score;
+}
+
