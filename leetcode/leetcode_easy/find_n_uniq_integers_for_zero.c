3116395626c7962f6bbe91747a6f82ca61afa9a0
TimBLukas <tim.b.lukas@gmail.com>
2026-07-27 13:26:27 +0200

    leetcode 1304: Find n unique integers sum up to zero

---
+++ leetcode/leetcode_easy/find_n_uniq_integers_for_zero.c
@@ -0,0 +1,42 @@
+/**
+* Leetcode 1304: Find N Uniqu Integers Sum up to Zero
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int* sumZero(int n, int* returnSize) 
+{
+        int* return_arr = malloc(n * sizeof(int));
+        int idx = 0;
+
+        if ( n % 2 == 0)
+        {
+                int start = -1 * (n / 2);
+                int end = n / 2;
+        
+
+                // skip 0
+                while ( start <= end )
+                {
+                        if ( start == 0)
+                                start++;
+                        else
+                                return_arr[idx++] = start++;
+                }
+        }
+        else
+        {
+                int start = -1 * (n / 2);
+                int end = n / 2;
+
+                // keep 0
+                while ( start <= end )
+                {
+                        return_arr[idx++] = start++;
+                }
+        }
+        *returnSize = idx;
+        return return_arr;
+}
+
