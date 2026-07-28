18d96da9435b5d1b86608f98ef0b1c780a7f5277
TimBLukas <tim.b.lukas@gmail.com>
2026-07-28 13:53:50 +0200

    leetcode 3898: Find the degree of each vertex

--- leetcode/leetcode_easy/find_degree_of_vertex.c
+++ leetcode/leetcode_easy/find_degree_of_vertex.c
@@ -2,11 +2,27 @@
  * Leetcode 3898: FInd the degree of each vertex
  */ 
 
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* findDegrees(int** matrix, int matrixSize, int* matrixColSize, int* returnSize) 
 {
-    
+        int* degrees = malloc(matrixSize * sizeof(int));
+        int cnt = 0;
+
+        for ( int i = 0; i < matrixSize; i++ ) {
+                for ( int j = 0; j < matrixColSize[i]; j++ ) {
+                        if ( matrix[i][j] == 1 )
+                                ++cnt;
+                }
+
+                degrees[i] = cnt;
+                cnt = 0;
+        }
+
+        *returnSize = matrixSize;
+        return degrees;
 }
