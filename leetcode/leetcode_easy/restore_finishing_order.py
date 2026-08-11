e896cfcb97205bc7f05e128e980dab865599b2b5
TimBLukas <tim.b.lukas@gmail.com>
2026-08-11 09:44:11 +0200

    Leetcode 3668: Restore Finishing order

---
+++ leetcode/leetcode_easy/restore_finishing_order.py
@@ -0,0 +1,23 @@
+"""
+Leetcode 3668: Restore Finishing order
+
+You are given an integer array order of length n and an integer array friends.
+
+- order contains every integer from 1 to n exactly once, representing the IDs of the participants of a race in their finishing order.
+- friends contains the IDs of your friends in the race sorted in strictly increasing order. Each ID in friends is guaranteed to appear in the order array.
+
+Return an array containing your friends' IDs in their finishing order.
+"""
+
+from typing import List
+
+
+class Solution:
+    def recoverOrder(self, order: List[int], friends: List[int]) -> List[int]:
+        tmp = set(friends)
+        final_order = []
+        for id in order:
+            if id in tmp:
+                final_order.append(id)
+
+        return final_order
