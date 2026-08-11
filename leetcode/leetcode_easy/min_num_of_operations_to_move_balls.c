4dca4e440e2ef2af6a9eed17d9052e2f7ab517ba
TimBLukas <tim.b.lukas@gmail.com>
2026-08-11 10:03:07 +0200

    Leetcode 1769: Minimum Number of Operations to Move All Ball to Each Box

---
+++ leetcode/leetcode_easy/min_num_of_operations_to_move_balls.c
@@ -0,0 +1,42 @@
+/**
+ * Leetcode 1769: Minimum Number of Operations to Move All Ball to Each Box
+ *
+ * You have n boxes. You are given a binary string boxes of length n, where
+ * boxes[i] is '0' if the ith box is empty, and '1' if it contains one ball.
+ *
+ * In one operation, you can move one ball from a box to an adjacent box. Box i
+ * is adjacent to box j if abs(i - j) == 1. Note that after doing so, there may
+ * be more than one ball in some boxes. Return an array answer of size n, where
+ * answer[i] is the minimum number of operations needed to move all the balls to
+ * the ith box. Each answer[i] is calculated considering the initial state of
+ * the boxes.
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int *minOperations(char *boxes, int *returnSize) {
+    int *result = malloc(strlen(boxes) * sizeof(int));
+    int *ball_positions = malloc(strlen(boxes) * sizeof(int));
+    int bp_i = 0;
+    int result_idx = 0;
+    int n = strlen(boxes);
+
+    for (int i = 0; i < n; i++)
+        if (boxes[i] != '0')
+            ball_positions[bp_i++] = i;
+
+    for (int i = 0; i < n; i++) {
+        result[i] = 0;
+        for (int j = 0; j < bp_i; j++) {
+            result[result_idx] += abs(ball_positions[j] - i);
+            printf("%d\n", abs(ball_positions[j] - i));
+        }
+        ++result_idx;
+    }
+
+    *returnSize = result_idx;
+    return result;
+}
