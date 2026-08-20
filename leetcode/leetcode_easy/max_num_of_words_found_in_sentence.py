7db22d3a1976844cc2d20cf2d7a368f8ea018c52
TimBLukas <tim.b.lukas@gmail.com>
2026-08-20 11:25:32 +0200

    Leetcode 2114: Maximum Number of words found in sentences

---
+++ leetcode/leetcode_easy/max_num_of_words_found_in_sentence.py
@@ -0,0 +1,14 @@
+"""
+Leetcode 2114: Maximum Number of Words found in sentences
+
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+Return the maximum number of words that appear in a single sentence.
+"""
+
+from typing import List
+
+
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        return max(map(lambda x: len(x.split(" ")), sentences))
