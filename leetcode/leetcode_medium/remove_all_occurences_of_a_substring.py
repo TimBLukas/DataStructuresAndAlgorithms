d96cc1ae7046eaf705d930a29c9e0904c4d46487
TimBLukas <tim.b.lukas@gmail.com>
2026-08-20 22:09:04 +0200

    Leetcode 3512: Minimum Operations to Make Array Sum Divisible by K

---
+++ leetcode/leetcode_medium/remove_all_occurences_of_a_substring.py
@@ -0,0 +1,13 @@
+"""
+Leetcode 1910: Remove all Occurrences of a Substring
+
+Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
+- Find the leftmost occurrence of the substring part and remove it from s.
+
+Return s after removing all occurrences of part.
+A substring is a contiguous sequence of characters in a string.
+"""
+
+
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str: ...
