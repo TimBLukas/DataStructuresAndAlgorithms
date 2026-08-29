915e6989389b5ca87cd97d1cfb9920dcbd8fd79d
Tim Lukas <tim.b.lukas@gmail.com>
2026-08-28 21:20:56 -0400

    Leetcode 2703: Return length of arguments passed

---
+++ leetcode/leetcode_easy/length_of_arguments_passed.js
@@ -0,0 +1,18 @@
+/**
+ * Leetcode 2703: Return length of arguments passed
+ * 
+ * Write a function argumentsLength that returns the count of arguments passed to it.
+ */
+
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
