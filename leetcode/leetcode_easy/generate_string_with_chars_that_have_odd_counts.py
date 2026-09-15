cf6c4ec217eed3e6548c3fa2a8548734f7a27d9e
TimBLukas <tim.b.lukas@gmail.com>
2026-09-15 10:17:05 +0200

    Leetcode 1374: Generate a String with characters that have odd counts

---
+++ leetcode/leetcode_easy/generate_string_with_chars_that_have_odd_counts.py
@@ -0,0 +1,14 @@
+"""
+Leetcode 1374: Generate a String With Characters That Have Odd Counts
+
+Given an integer n, return a string with n characters such that each character
+in such string occurs an odd number of times.
+
+The returned string must contain only lowercase English letters.
+If there are multiples valid strings, return any of them
+"""
+
+
+class Solution:
+    def generateTheString(self, n: int) -> str:
+        return "a" * (n - 1) + "b" if n % 2 == 0 else "a" * n
