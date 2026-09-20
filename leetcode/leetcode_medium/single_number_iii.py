3b8761394660ed75e48d01a2f790b21aabe31c91
TimBLukas <tim.b.lukas@gmail.com>
2026-09-20 12:17:54 +0200

    Leetcode 260: Single Number III

---
+++ leetcode/leetcode_medium/single_number_iii.py
@@ -0,0 +1,21 @@
+"""
+Leetcode 260: Single Number III
+
+Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice.
+Find the two elements that appear only once. You can return the answer in any order.
+
+You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+"""
+
+from collections import Counter
+
+
+class Solution:
+    def singleNumber(self, nums: list[int]) -> list[int]:
+        result = []
+        c = Counter(nums)
+        for i, count in c.items():
+            if count == 1:
+                result.append(i)
+
+        return result
